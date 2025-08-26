912238edaa7a5b0a2b33a4ffca6ce65f5ce97afa
Marta <martadelfino23@gmail.com>
2025-08-26 19:37:49 +0100

    current applet, with resources, but not working

--- src/exomiser.sh
+++ src/exomiser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # exomiser first
+set -e -x -o pipefail
 
 main() {
 
@@ -7,49 +8,30 @@ main() {
     echo "Value of analysis_file: '$analysis_file'"
     echo "Value of vcf: '$vcf'"
     echo "Value of config: '$config'"
-    echo "Value of genome_data: '$genome_data'"
-    echo "Value of phenotype_data: '$phenotype_data'" 
-
-    # The following line(s) use the dx command-line tool to download your file
-    # inputs to the local file system using variable names for the filenames. To
-    # recover the original filenames, you can use the output of "dx describe
-    # "$variable" --name".
+#    echo "Value of genome_data (dir): '$genome_data'"
+#    echo "Value of phenotype_data (dir): '$phenotype_data'" 
 
     dx download "$docker_image" -o docker_image
     dx download "$config" -o application.properties
     dx download "$analysis_file" -o analysis_file
     dx download "$vcf" -o vcf
-    dx download "$genome_data" -o genome_data.zip
-    dx download "$phenotype_data" -o phenotype_data.zip
+#    dx download "$genome_data" -o genome_data.zip
+#    dx download "$phenotype_data" -o phenotype_data.zip
+  
+#    echo "Downloading genome and phenotype directories"
+#    dx download "$genome_data" -o genome_data --recursive
+#    dx download "$phenotype_data" -o phenotype_data --recursive
 
-    echo "Preparing Exomiser data"
-    mkdir -p /exomiser-data
+#    echo "Preparing Exomiser data directory"
+#    mkdir -p /exomiser-data 
+#    cp -r genome_data/* /exomiser-data/
+#    cp -r phenotype_data/* /exomiser-data/
 
-    echo "Unzipping genome data"
-    unzip -q genome_data.zip -d /exomiser-data
-
-    echo "Unzipping phenotype data" 
-    unzip -q phenotype_data.zip -d /exomiser-data
-
-    echo "Data contents:"
-    ls -lh /exomiser-data
+    echo "Exomiser data in resources directory:"
+    ls -lh /resources
 
     # Fixing permissions so Exomiser's 'nonroot' user can read   
-    chmod -R a+rX /exomiser-data
-
-    # Fill in your application code here.
-    #
-    # To report any recognized errors in the correct format in
-    # $HOME/job_error.json and exit this script, you can use the
-    # dx-jobutil-report-error utility as follows:
-    #
-    #   dx-jobutil-report-error "My error message"
-    #
-    # Note however that this entire bash script is executed with -e
-    # when running in the cloud, so any line which returns a nonzero
-    # exit code will prematurely exit the script; if no error was
-    # reported in the job_error.json file, then the failure reason
-    # will be AppInternalError with a generic error message.
+    chmod -R a+rX /resources
     
     docker load < docker_image
     
@@ -59,27 +41,16 @@ main() {
     echo "Running Exomiser"
     docker run --rm \
         -v /home/dnanexus:/home/dnanexus \
-        -v /exomiser-data:/exomiser-data \
+        -v /resources:/exomiser-data \
         exomiser/exomiser-cli:14.0.0-distroless \
         --analysis /home/dnanexus/analysis_file \
-        --output-directory /home/dnanexus/results \
+        --output-directory "$outdir" \
         --exomiser.data-directory=/exomiser-data \
         --spring.config.location=/home/dnanexus/application.properties
 
-                            
-    # The following line(s) use the dx command-line tool to upload your file
-    # outputs after you have created them on the local file system.  It assumes
-    # that you have used the output field name for the filename for each output,
-    # but you can change that behavior to suit your needs.  Run "dx upload -h"
-    # to see more options to set metadata.
-
+    echo "Uploading results"
     html=$(dx upload "$outdir"/*.html --brief)
     json=$(dx upload "$outdir"/*.json --brief)
-
-    # The following line(s) use the utility dx-jobutil-add-output to format and
-    # add output variables to your job's output as appropriate for the output
-    # class.  Run "dx-jobutil-add-output -h" for more information on what it
-    # does.
 
     dx-jobutil-add-output html "$html" --class=file
     dx-jobutil-add-output json "$json" --class=file
